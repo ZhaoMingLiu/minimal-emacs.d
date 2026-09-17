682118d63bcc7d51c31ea05bdf9951bed9a6a816
ZhaoMingLiu <liu125927137@gmail.com>
2026-09-17 23:19:42 +0800

    split new my-vc.el file

--- post-init.el
+++ post-init.el
@@ -57,21 +57,7 @@
 (require 'my-dired)
 
 ;;; VC
-(use-package magit
-  :bind
-  ("C-x g" . magit-status))
-
-
-(use-package diff-hl
-  :commands (diff-hl-mode
-             global-diff-hl-mode)
-  :hook (prog-mode . diff-hl-mode)
-  :init
-  (setq diff-hl-flydiff-delay 0.4)  ; Faster
-  (setq diff-hl-show-staged-changes nil)  ; Realtime feedback
-  (setq diff-hl-update-async t)  ; Do not block Emacs
-  (setq diff-hl-global-modes '(not pdf-view-mode image-mode)))
-
+(require 'my-vc)
 
 ;;; TTY
 (require 'my-tty)
