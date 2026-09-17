55b1d0cc33ad53ce37e7fa7b4b0169b514c5eef5
ZhaoMingLiu <liu125927137@gmail.com>
2026-09-17 23:14:45 +0800

    update tty for windows

--- user-lisp/hacks/my-tty.el
+++ user-lisp/hacks/my-tty.el
@@ -2,11 +2,13 @@
 
 (use-package ghostel
   :ensure t
-  :bind (("C-x m" . ghostel)
+  :bind (
+         ;; ("C-x m" . ghostel)
          :map ghostel-mode-map
          ("C-c l" . ghostel-clear-scrollback))
   :config
-  (setq ghostel-shell '("powershell.exe")))
+  (when (eq system-type 'windows-nt)
+    (setq ghostel-shell '("powershell.exe" "-NoLogo" "-ExecutionPolicy" "Bypass"))))
 
 (use-package ghostel-eshell
   :ensure nil
@@ -19,6 +21,19 @@
 (use-package ghostel-comint
   :ensure nil
   :hook (after-init . ghostel-comint-global-mode))
+
+
+(use-package consult-ghostel
+  :vc (:url "https://github.com/dakra/ghostel"
+            :lisp-dir "extensions/consult-ghostel"
+            :rev :newest)
+  :after (ghostel consult)
+  :demand t
+  :bind (("C-x m" . consult-ghostel)
+         :map project-prefix-map
+         ("m" . consult-ghostel-project)
+         :map ghostel-semi-char-mode-map
+         ("C-c h" . consult-ghostel-history)))
 
 
 (provide 'my-tty)
